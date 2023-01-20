a4d3f9116d03e545338be76a67da0eb0414f167a
hitzza <cslzh4433@gmail.com>
2023-01-20 16:21:20 +0900

    header icon,css1

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -5,11 +5,14 @@ const Header =
     console.log(
       'header',
     );
+    //TODO: 메인 아이콘클릭할 경우 a 태그로 index.js로 이동하는것아 아닌 props로 Restaurants.js에서 메인화면 상태로 보여주는 state를 반환해서 해야함.
     return (
       <header>
         <section>
           <h2>
-            header
+            <a href="./index.js">
+              맛집지도
+            </a>
           </h2>
         </section>
       </header>
--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Restaurants from './components/Restaurants.js';
 import './styles/style.css';
 
-ReactDOM.render(
+const root =
+  ReactDOM.createRoot(
+    document.getElementById(
+      'restaurants',
+    ),
+  );
+
+root.render(
   <Restaurants />,
-  document.querySelector(
-    '#restaurants',
-  ),
 );
 console.log(
   'a',
