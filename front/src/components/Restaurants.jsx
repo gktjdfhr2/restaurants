6aa71389693928a3cd2f22f16df9be2152edfbb6
hitzza <cslzh4433@gmail.com>
2023-01-25 22:43:59 +0900

    modify header map css

--- front/src/components/Restaurants.jsx
+++ front/src/components/Restaurants.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import MainBody from '../pages/KakaoMap';
+import MainBody from '../pages/MainBody';
 
 export default class Restaurants extends React.Component {
   constructor() {
