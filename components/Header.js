2387423ebb3920a95590d710192a5b886caf4ad8
hitzza <cslzh4433@gmail.com>
2023-01-26 16:51:18 +0900

    add icons;

--- components/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Header = () => {
-  console.log('header');
-  return /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("h2", null, "header"));
-};
-export default Header;
