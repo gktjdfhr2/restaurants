06a757920cf9711c045ddc67dca0d48a04a7ddfd
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-01-29 19:46:05 +0900

    feat: #2 프로젝트 생성

---
+++ backend/src/test/java/com/worst/restmap/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worst.restmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
