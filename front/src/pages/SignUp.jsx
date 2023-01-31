57c5f3144bec10771b36c522f0c5586166353637
hitzza <cslzh4433@gmail.com>
2023-01-31 19:22:39 +0900

    modify SignUp page items & css

--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function SignUp() {
-  return <div id="dummy">회원가입 페이지</div>;
+  return (
+    <section id="SignUpSection">
+      <div id="SignUpTitle">회원가입</div>
+      <div id="SignUpItemContainer">
+        <div>아이디</div>
+        <input type="text" />
+        <div id="idState">아이디를 입력해주세요</div>
+        <div>비밀번호</div>
+        <input type="text" />
+        <input type="text" />
+        <div id="pwState">비밀번호를 확인해주세요</div>
+        <div>이름</div>
+        <input type="text" />
+        <div>주소</div>
+        <input type="text" />
+        <div>전화번호</div>
+        <input type="text" />
+        <div>회원 유형</div>
+        <select defaultValue="고객">
+          <option value="고객">고객</option>
+          <option value="사업자">사업자</option>
+        </select>
+        <input type="button" id="infoSubmit" value="가입하기" />
+      </div>
+    </section>
+  );
 }
 
 export default SignUp;
