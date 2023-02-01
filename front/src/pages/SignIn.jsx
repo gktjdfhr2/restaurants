01172926c01d869a466094bc47c6c07a510305fd
hitzza <cslzh4433@gmail.com>
2023-02-01 17:23:32 +0900

    signup page add script

--- front/src/pages/SignIn.jsx
+++ front/src/pages/SignIn.jsx
@@ -10,17 +10,19 @@ function SignIn() {
         <div>맛집지도</div>
       </section>
       <section id="SignInInfo">
-        <div>
-          <input type="text" id="SignInId" />
-          <input type="text" id="SignInPw" />
-        </div>
-        <div id="SignInSubmitForm">
-          <input type="button" id="SignInSubmit" value="로그인" />
-          <div id="signUp">
-            회원이 아니신가요?
-            <Link to="/SignUp">회원가입하기</Link>
+        <form>
+          <div>
+            <input type="text" id="SignInId" />
+            <input type="password" id="SignInPw" />
           </div>
-        </div>
+          <div id="SignInSubmitForm">
+            <input type="button" id="SignInSubmit" value="로그인" />
+            <div id="signUp">
+              회원이 아니신가요?
+              <Link to="/SignUp">회원가입하기</Link>
+            </div>
+          </div>
+        </form>
       </section>
     </>
   );
--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -1,29 +1,60 @@
 import React from 'react';
 
 function SignUp() {
+  const signInHandleChagne = () => {
+    const showTarget = document.querySelector('#idState');
+    const pwShowTarget = document.querySelector('#pwState');
+    const pw1 = document.querySelector('#pw1').value;
+    const pw2 = document.querySelector('#pw2').value;
+    const userId = document.querySelector('#userId').value;
+    console.log('change');
+    if (userId.length > 0) {
+      showTarget.setAttribute('style', 'color: white;');
+    } else {
+      showTarget.setAttribute('style', 'color: black;');
+    }
+
+    if (pw1 === pw2) {
+      pwShowTarget.setAttribute('style', 'color: white;');
+    } else {
+      pwShowTarget.setAttribute('style', 'color: red;');
+    }
+  };
+  const signIn = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
+
   return (
     <section id="SignUpSection">
       <div id="SignUpTitle">회원가입</div>
       <div id="SignUpItemContainer">
-        <div>아이디</div>
-        <input type="text" />
-        <div id="idState">아이디를 입력해주세요</div>
-        <div>비밀번호</div>
-        <input type="text" />
-        <input type="text" />
-        <div id="pwState">비밀번호를 확인해주세요</div>
-        <div>이름</div>
-        <input type="text" />
-        <div>주소</div>
-        <input type="text" />
-        <div>전화번호</div>
-        <input type="text" />
-        <div>회원 유형</div>
-        <select defaultValue="고객">
-          <option value="고객">고객</option>
-          <option value="사업자">사업자</option>
-        </select>
-        <input type="button" id="infoSubmit" value="가입하기" />
+        <form onSubmit={signIn} onChange={signInHandleChagne}>
+          <div>아이디</div>
+          <input type="text" id="userId" placeholder="아이디" />
+          <div id="idState">아이디를 입력해주세요</div>
+          <div>비밀번호</div>
+          <input type="password" id="pw1" placeholder="비밀번호" />
+          <input type="password" id="pw2" placeholder="비밀번호 확인" />
+          <div id="pwState">비밀번호를 확인해주세요</div>
+          <div>이름</div>
+          <input type="text" placeholder="예) 홍길동" />
+          <div>주소</div>
+          <input type="text" />
+          <div>전화번호</div>
+          <input
+            type="text"
+            placeholder="-을 제거해서 입력해주세요 예)01012345678"
+          />
+          <div>회원 유형</div>
+          <select defaultValue="고객">
+            <option value="고객">고객</option>
+            <option value="사업자">사업자</option>
+          </select>
+          <button type="submit" id="infoSubmit">
+            가입하기
+          </button>
+        </form>
       </div>
     </section>
   );
