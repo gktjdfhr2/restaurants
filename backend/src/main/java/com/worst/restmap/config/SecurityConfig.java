9084f2806228faa7a0287ab468170fe086f029d6
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-02-06 01:56:26 +0900

    Feat: SpringSecurity login환경 설정

--- backend/src/main/java/com/worst/restmap/config/SecurityConfig.java
+++ backend/src/main/java/com/worst/restmap/config/SecurityConfig.java
@@ -1,8 +1,27 @@
 package com.worst.restmap.config;
 
-import org.springframework.context.annotation.Bean;
+import com.worst.restmap.config.jwt.JwtAccessDeniedHandler;
+import com.worst.restmap.config.jwt.JwtAuthenticationEntryPoint;
+import com.worst.restmap.config.jwt.TokenProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.filter.CorsFilter;
 
+@EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+    public SecurityConfig(TokenProvider tokenProvider, CorsFilter corsFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 }
