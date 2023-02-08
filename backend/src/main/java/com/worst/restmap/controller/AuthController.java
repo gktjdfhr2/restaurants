228093fba91ab7da66d6f2b6f8a3f921e0f28daf
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-02-08 23:56:45 +0900

    fix: cor 오류 수정

--- backend/src/main/java/com/worst/restmap/controller/AuthController.java
+++ backend/src/main/java/com/worst/restmap/controller/AuthController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @GetMapping("/signUp")
--- front/src/components/SigninInput.jsx
+++ front/src/components/SigninInput.jsx
@@ -18,7 +18,7 @@ function SigninInput() {
 
   const register = () => {
     axios
-      .post('localhost:8080/api/auth/signIn', signInCheck)
+      .post('http://localhost:8080/api/auth/signIn', signInCheck)
       .then((response) => {
         console.log(response);
         console.log('data:', response.data.user);
