ddfae68b246dc32248ca678bcedffc3ed297ccd5
hitzza <cslzh4433@gmail.com>
2023-02-08 21:32:48 +0900

    proprietorMainBody add items

--- front/src/pages/ProprietorMainBody.jsx
+++ front/src/pages/ProprietorMainBody.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 function ProprietorMainBody() {
-  return <div id="dummy">ProprietorMainBody</div>;
+  return (
+    <section className="proprietorSection">
+      <div>
+        <div>대기 목록</div>
+        <ul className="flexUl">
+          <li className="flexLi">
+            <ul>
+              <li>47</li>
+              <li>18 : 20</li>
+            </ul>
+            <ul>
+              <li>정한결</li>
+              <li>성인 03 유아 00 | 4782</li>
+            </ul>
+            <ul>
+              <li>호출 아이콘</li>
+              <li>18 : 39</li>
+            </ul>
+            <ul>
+              <li>대기 아이콘</li>
+              <li>대기중</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default ProprietorMainBody;
