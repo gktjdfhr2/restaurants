5e304889af20ffa2d6c848e4e7615bdd78a9bedc
hitzza <cslzh4433@gmail.com>
2023-02-09 19:32:41 +0900

    propritorMainBody refactoring add state

--- front/src/pages/ProprietorMainBody.jsx
+++ front/src/pages/ProprietorMainBody.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropritorStayList from '../components/PropritorStayList';
 
 function ProprietorMainBody() {
+  const [showState, setShowState] = useState({
+    stayListShow: true,
+    histListShow: false,
+  });
+  function showStayButton() {
+    console.log('showStayButton');
+    setShowState({
+      stayListShow: !showState.stayListShow,
+      histListShow: !showState.histListShow,
+    });
+  }
+  function showHisyButton() {
+    console.log('showHistButton');
+    setShowState({
+      histListShow: !showState.histListShow,
+      stayListShow: !showState.stayListShow,
+    });
+  }
   return (
     <section className="proprietorSection">
       <div>
-        <div>대기 목록</div>
-        <PropritorStayList />
-        <PropritorStayList />
+        <div className="stayListTitle">
+          대기 목록
+          <button
+            type="button"
+            className="closeButton"
+            onClick={showStayButton}
+          >
+            show
+          </button>
+        </div>
+        {showState.stayListShow && (
+          <>
+            <PropritorStayList />
+            <PropritorStayList />
+          </>
+        )}
+        <div className="stayListTitle">
+          히스토리
+          <button
+            type="button"
+            className="closeButton"
+            onClick={showHisyButton}
+          >
+            show
+          </button>
+        </div>
+        {showState.histListShow && (
+          <>
+            <div>hist</div>
+            <div>items</div>
+          </>
+        )}
       </div>
     </section>
   );
