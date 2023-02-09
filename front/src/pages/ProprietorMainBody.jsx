d18c2320dfcf4e4b56bf78446cc5df9ad62806a5
hitzza <cslzh4433@gmail.com>
2023-02-09 17:01:39 +0900

    add proprietorMainBody stay items2

--- front/src/pages/ProprietorMainBody.jsx
+++ front/src/pages/ProprietorMainBody.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 function ProprietorMainBody() {
+  function callClick() {
+    console.log('call');
+  }
+  function stayingClick() {
+    console.log('staying');
+  }
   return (
     <section className="proprietorSection">
       <div>
@@ -16,12 +22,64 @@ function ProprietorMainBody() {
               <li className="bottomLi stayPersonnel">성인 03 유아 00 | 4782</li>
             </ul>
             <ul>
-              <li className="topLi">호출 아이콘</li>
-              <li className="bottomLi">18 : 39</li>
+              <li className="topLi">
+                <button
+                  type="button"
+                  className="callButton"
+                  onClick={callClick}
+                >
+                  호출
+                </button>
+              </li>
+              <li className="bottomLi recentCall">18 : 39</li>
             </ul>
             <ul>
-              <li className="topLi">대기 아이콘</li>
-              <li className="bottomLi">대기중</li>
+              <li className="topLi">
+                <button
+                  type="button"
+                  className="stayingButton"
+                  onClick={stayingClick}
+                >
+                  대기
+                </button>
+              </li>
+              <li className="bottomLi stayState">대기중</li>
+            </ul>
+          </li>
+        </ul>
+        <ul className="flexUl">
+          <li className="flexLi">
+            <ul>
+              <li className="topLi stayNum">47</li>
+              <li className="bottomLi stayStart">18 : 20</li>
+            </ul>
+            <ul>
+              <li className="topLi stayName">정한결</li>
+              <li className="bottomLi stayPersonnel">성인 03 유아 00 | 4782</li>
+            </ul>
+            <ul>
+              <li className="topLi">
+                <button
+                  type="button"
+                  className="callButton"
+                  onClick={callClick}
+                >
+                  호출
+                </button>
+              </li>
+              <li className="bottomLi recentCall">18 : 39</li>
+            </ul>
+            <ul>
+              <li className="topLi">
+                <button
+                  type="button"
+                  className="stayingButton"
+                  onClick={stayingClick}
+                >
+                  대기
+                </button>
+              </li>
+              <li className="bottomLi stayState">대기중</li>
             </ul>
           </li>
         </ul>
