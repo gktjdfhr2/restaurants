9efdefec78bd366dcd3bda88e9543fd68d1c22e2
jeahaChoi <jaeha-choi@nhn-commerce.com>
2023-02-21 21:29:15 +0900

    feat: css 기초 설정

--- client/src/UI/Layouts/DefaultLayout.tsx
+++ client/src/UI/Layouts/DefaultLayout.tsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const DefaultLayout = styled.main`
   width: 1024px;
-  height: 200vh;
+  height: 100vh;
   margin: 0 auto;
   border: 1px solid black;
+  padding: 0 40px;
 `;
 
 export default DefaultLayout;
--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,11 +1,10 @@
-import TextInput from '@UI/Form/TextInput';
 import DefaultLayout from '@UI/Layouts/DefaultLayout';
 import HeaderLayout from '@UI/Layouts/HeaderLayout';
 
 const TestPage = () => {
   return (
     <>
-      <HeaderLayout />
+      <HeaderLayout></HeaderLayout>
       <DefaultLayout></DefaultLayout>
     </>
   );
