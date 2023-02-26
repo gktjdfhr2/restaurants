40f78ee14dc768575e3f79cf7ffffa189fed9ef3
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-02-26 20:56:00 +0900

    Fix: SSE 컨트롤러 생성

---
+++ backend/src/main/java/com/worst/restmap/controller/SseController.java
@@ -0,0 +1,36 @@
+package com.worst.restmap.controller;
+
+import com.worst.restmap.component.SseEmitters;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sse")
+@Slf4j
+public class SseController {
+
+    private final SseEmitters sseEmitters;
+
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect() {
+        SseEmitter emitter = new SseEmitter();
+        sseEmitters.add(emitter);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data("connected!"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
+    }
+}
