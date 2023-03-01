8850cf37a55fea44559019360db8060f2e3b55e4
hitzza <cslzh4433@gmail.com>
2023-03-01 23:30:29 +0900

    회원가입

--- client/src/customer/pages/SignUpPage.tsx
+++ client/src/customer/pages/SignUpPage.tsx
@@ -1,34 +1,34 @@
-import { ChangeEvent, useState } from 'react';
-import DefaultLayout from '@customer/UI/Layouts/DefaultLayout';
-import HeaderLayout from '@customer/UI/Layouts/HeaderLayout';
-import HeaderBar from '@customer/components/HeaderBar';
-import SignUpPart from '@customer/components/SignUpPart';
+// import { ChangeEvent, useState } from 'react';
+// import DefaultLayout from '@customer/UI/Layouts/DefaultLayout';
+// import HeaderLayout from '@customer/UI/Layouts/HeaderLayout';
+// import HeaderBar from '@customer/components/HeaderBar';
+// import SignUpPart from '@customer/components/SignUpPart';
 
-const SignUpPage = () => {
-  const [memberData, setMemberData] = useState({
-    memberRole: 0,
-    memberEmail: '',
-    memberPassword: '',
-    memberName: '',
-    memberPhone: '',
-    memberAddress: '',
-  });
+// const SignUpPage = () => {
+//   const [memberData, setMemberData] = useState({
+//     memberRole: 0,
+//     memberEmail: '',
+//     memberPassword: '',
+//     memberName: '',
+//     memberPhone: '',
+//     memberAddress: '',
+//   });
 
-  const idHandle = (event: ChangeEvent) => {
-    console.log((event.target as HTMLInputElement).value);
-    const value = (event.target as HTMLInputElement).value;
-    // setMemberData( memberData.memberEmail:value);
-  };
-  return (
-    <>
-      <HeaderLayout>
-        <HeaderBar />
-      </HeaderLayout>
-      <DefaultLayout>
-        <SignUpPart />
-      </DefaultLayout>
-    </>
-  );
-};
+//   const idHandle = (event: ChangeEvent) => {
+//     console.log((event.target as HTMLInputElement).value);
+//     const value = (event.target as HTMLInputElement).value;
+//     // setMemberData( memberData.memberEmail:value);
+//   };
+//   return (
+//     <>
+//       <HeaderLayout>
+//         <HeaderBar />
+//       </HeaderLayout>
+//       <DefaultLayout>
+//         <SignUpPart />
+//       </DefaultLayout>
+//     </>
+//   );
+// };
 
-export default SignUpPage;
+// export default SignUpPage;
