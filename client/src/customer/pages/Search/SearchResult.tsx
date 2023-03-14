5380369e86868e14301d4f0b25b5868206f31f82
hitzza <cslzh4433@gmail.com>
2023-03-14 20:00:55 +0900

    별점 작업

--- client/src/customer/pages/Search/SearchResult.tsx
+++ client/src/customer/pages/Search/SearchResult.tsx
@@ -1,6 +1,88 @@
 import styled from 'styled-components';
 
+const ResultContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+`;
+const ResultItems = styled.div`
+  display: flex;
+  width: 100%;
+  height: 124px;   
+  justify-content: space-between;
+  align-items: center;
+  margin :20px 0;
+}
+`;
+
+const StorePicture = styled.div`
+  flex-grow: 0.2;
+  width: initial;
+  height: inherit;
+  background: url(/src/assets/images/placeholder.png) no-repeat;
+  background-size: contain;
+`;
+
+const StoreInformation = styled.div`
+  flex-grow: 0.8;
+`;
+const StoreTitle = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+const StoreReviewContainer = styled.div`
+  display: flex;
+  height: 20px;
+  width: 100%;
+  margin: 5px 0;
+  background-color: gray;
+`;
+
+const ReviewScore = styled.div`
+  height: 20px;
+  width: 100%;
+`;
+
+const ScoreBackground = styled.span`
+  display: inline-block;
+  height: 20px;
+  width: 100px;
+  background: url(/src/assets/images/rating_0.png) no-repeat;
+  background-size: contain;
+`;
+const CurrentScore = styled.span`
+  display: inline-block;
+  height: 20px;
+  width: 100%;
+  //TODO:props로 점수 받아서 width % 결정
+  background: url(/src/assets/images/rating_1.png) no-repeat;
+  background-size: 100px 20px;
+`;
+
+const TotalScore = styled.span`
+  height: 20px;
+  display: inline-block;
+`;
+
 const SearchResult = () => {
-  return <div>gi</div>;
+  return (
+    <ResultContainer>
+      <ResultItems>
+        <StorePicture />
+        <StoreInformation>
+          <StoreTitle>소우데스</StoreTitle>
+          <StoreReviewContainer>
+            <ReviewScore>
+              <ScoreBackground>
+                <CurrentScore />
+              </ScoreBackground>
+              <TotalScore>123</TotalScore>
+            </ReviewScore>
+          </StoreReviewContainer>
+        </StoreInformation>
+      </ResultItems>
+    </ResultContainer>
+  );
 };
 export default SearchResult;
