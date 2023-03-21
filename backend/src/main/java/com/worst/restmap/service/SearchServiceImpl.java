dabb0b9f218f83149cd925a8e2ea900ee1d9c4c5
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2023-03-21 10:50:42 +0900

    gktjdfhr2
    Feat: 검색기록 추가

--- backend/src/main/java/com/worst/restmap/service/SearchServiceImpl.java
+++ backend/src/main/java/com/worst/restmap/service/SearchServiceImpl.java
@@ -6,10 +6,8 @@ import com.worst.restmap.domain.dto.SearchDto;
 import com.worst.restmap.domain.dto.member.SearchStoreDto;
 import com.worst.restmap.domain.entity.Business;
 import com.worst.restmap.domain.entity.Search;
-import com.worst.restmap.repository.BusinessRepository;
-import com.worst.restmap.repository.BusinessTagRepository;
-import com.worst.restmap.repository.MemberRepository;
-import com.worst.restmap.repository.TagRepository;
+import com.worst.restmap.repository.*;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +22,16 @@ public class SearchServiceImpl implements SearchService{
     private final MemberRepository memberRepository;
     private final BusinessRepository businessRepository;
     private final BusinessTagRepository businessTagRepository;
+    private final SearchRepository searchRepository;
     private final TagRepository tagRepository;
     @Override
+    @Transactional
     public ResponseEntity<StatusCode> findStore(String memberEmail, SearchDto searchDto) {
         LocalDateTime localDateTime = LocalDateTime.now();
         String businessName = searchDto.getKeyWord();
         List<Business> business = businessRepository.findByBusinessNameContaining(businessName);
         Search search = Search.builder().searchMemberEmail(memberEmail).searchKeyword(businessName).searchLogDatetime(localDateTime).build();
+        searchRepository.save(search);
         return new JsonResponse().send(HttpStatus.OK, StatusCode.builder().resCode("").resMsg("정상조회").data(business).build());
     }
 
