f524357255c6e2015605c65c06b5ec3b1b53e27f
hitzza <cslzh4433@gmail.com>
2023-03-23 17:39:09 +0900

    로그인 쿠키 유효시간 설정

--- client/src/customer/pages/SignIn/index.tsx
+++ client/src/customer/pages/SignIn/index.tsx
@@ -55,8 +55,15 @@ const SignIn = () => {
       .then((response) => {
         console.log('data :', response);
         console.log('headers :', response.headers);
+        const expires = new Date();
+        expires.setMinutes(expires.getMinutes() + 30);
 
-        setCookie('token', response.headers.authorization);
+        setCookie('token', response.headers.authorization, {
+          path: '/',
+          expires, // 유효 시간
+          secure: true, // 웹 브라우저와 웹 서버가 https로 통신하는 경우에만 쿠키 저장
+          httpOnly: true, // document.cookie라는 자바스크립트 코드로 쿠키에 비정상적으로 접속하는 것을 막는 옵션
+        });
         //TODO: 쿠키 refresh  추가
         navigate('/');
       })
