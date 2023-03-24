a68e7d9be7c936dccb5411f598aa1d2f553ade0a
hitzza <cslzh4433@gmail.com>
2023-03-24 16:25:49 +0900

    주소 받아와서 지도 세팅

--- client/src/customer/components/KakaoMap.tsx
+++ client/src/customer/components/KakaoMap.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
+  const [location, setLocation] = useState({
+    lat: 33.450701,
+    lng: 126.570667,
+  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      setLocation(() => ({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }));
+    });
+  }, []);
+
   return (
     <Map // 지도를 표시할 Container
       center={{
         // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
+        lat: location.lat,
+        lng: location.lng,
       }}
       style={{
         // 지도의 크기
